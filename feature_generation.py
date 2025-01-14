c5045ad3949c0a1cb861791899c8a9e8d03293e2
Ludvig <ludvig@clin.au.dk>
2025-01-14 15:27:35 +0100

    Allows passing bam file paths directly. Refactors running of commands.
    NOTE: Breaks specifying output folder as input for running on preprocessed features

--- feature_generation.py
+++ feature_generation.py
@@ -1,41 +1,38 @@
+import argparse
 import os
 import subprocess
 import numpy as np
-import pandas as pd
 import pickle
-import copy, sys
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--input', type=str, nargs="*", required=True, help='Path(s) to one or more `.bam` files.')
+parser.add_argument('--output', type=str, required=True, help='Output folder path where the Fragle predictions and processed features can be found')
+parser.add_argument('--cpu', type=int, required=True, help='Number of CPUs to use for parallel processing of bam files')
+parser.add_argument('--bin_locations', type=str, required=True, help='Path to file with bin locations.')
+args = parser.parse_args()
 
-input_folder = sys.argv[1]
-output_folder = sys.argv[2]
-CPU = sys.argv[3]
-bin_locations = sys.argv[4]
-if os.path.exists(output_folder)==False:
-  os.mkdir(output_folder)
+sample_cnt = len(args.input)
 
-sample_cnt = 0
-for file_ in os.listdir(input_folder):
-  if file_.endswith('.bam'):
-    sample_cnt += 1
+if not os.path.exists(args.output):
+  os.mkdir(args.output)
+
 data = np.zeros((sample_cnt, 350))
 data_meta = []
 
 i = 0
-for file_ in os.listdir(input_folder):
-  if file_.endswith('.bam'):
-    bamfile = f'{input_folder}/{file_}'
-    output_path = f'{output_folder}/{file_[:-4]}.npy'
-    print(i+1, file_)
-    data_meta.append(file_[:-4])
-    command = f'python sample_feature_generation.py {bamfile} {output_path} {CPU} {bin_locations}'
-    subprocess.run(command, shell=True)
-    data[i]= np.load(output_path)
-    os.remove(output_path)
-    i += 1
+for file_ in args.input:
+  output_path = f'{args.output}/{file_[:-4]}.npy'
+  print(i+1, file_)
+  data_meta.append(file_[:-4])
+  command = f'python sample_feature_generation.py {file_} {output_path} {args.cpu} {args.bin_locations}'
+  subprocess.run(command, shell=True)
+  data[i] = np.load(output_path)
+  os.remove(output_path)
+  i += 1
 
 data_dict = {}
 data_dict['samples'] = data
 data_dict['meta_info'] = data_meta
-output_file = f'{output_folder}/data.pkl' 
+output_file = f'{args.output}/data.pkl' 
 with open(output_file, 'wb') as f:
   pickle.dump(data_dict, f)
